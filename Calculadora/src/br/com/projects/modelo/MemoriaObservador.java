d464e6b3e3e8314da5c2dc7842ed9d9cc2f8ca15
Mario23junior <48899365+Mario23junior@users.noreply.github.com>
2020-06-14 03:50:17 -0300

    Add files via upload

---
+++ Calculadora/src/br/com/projects/modelo/MemoriaObservador.java
@@ -0,0 +1,7 @@
+package br.com.projects.modelo;
+
+ public interface MemoriaObservador {
+   
+	  public void valorAlterado(String novoValor);
+	
+}
---
+++ Calculadora/src/br/com/projects/visao/Display.java
@@ -0,0 +1,48 @@
+package br.com.projects.visao;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import br.com.projects.modelo.Memoria;
+import br.com.projects.modelo.MemoriaObservador;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel implements MemoriaObservador{
+     
+	private final JLabel label;
+ 	
+	public Display() {
+		Memoria.getInstancia().adicionarObservador(this);
+		
+		setBackground(new Color( 46, 49, 50));
+ 		label = new JLabel(Memoria.getInstancia().getTextoAtual());
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Courier", Font.PLAIN, 40));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10,25));
+		add(label);
+ 	}
+	
+	@Override
+	public void valorAlterado(String novoValor) {
+ 	    label.setText(novoValor);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
